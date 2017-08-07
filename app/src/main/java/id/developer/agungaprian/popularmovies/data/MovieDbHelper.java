bb95f2eb7a032a1f24387590aebdf162904630b8
agungaprian <agungaprianto96@gmail.com>
2017-08-07 19:22:11 +0700

    add provider

--- app/src/main/java/id/developer/agungaprian/popularmovies/data/MovieDbHelper.java
+++ app/src/main/java/id/developer/agungaprian/popularmovies/data/MovieDbHelper.java
@@ -1,8 +1,54 @@
 package id.developer.agungaprian.popularmovies.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+import static id.developer.agungaprian.popularmovies.data.MovieDatabaseContract.Movies.TABLE_NAME;
 /**
  * Created by agungaprian on 03/08/17.
  */
 
-public class MovieDbHelper {
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+    /**
+     * Database helper used to work together with {@link MovieDatabaseContract}
+     */
+        private static final String DB_NAME = "movies.db";
+        private static final int DB_VERSION = 1;
+        private final Context context;
+
+        MovieDbHelper(Context context) {
+            super(context, DB_NAME, null, DB_VERSION);
+            this.context = context;
+        }
+
+        @Override public void onCreate(SQLiteDatabase db) {
+
+            db.execSQL("CREATE TABLE " + TABLE_NAME + "("
+                    + BaseColumns._ID + " INTEGER NOT NULL PRIMARY KEY,"
+                    + MovieDatabaseContract.Movies.COLUMN_MOVIE_ID + " TEXT NOT NULL,"
+                    + MovieDatabaseContract.Movies.COLUMN_MOVIE_TITLE + " TEXT NOT NULL,"
+                    + MovieDatabaseContract.Movies.COLUMN_MOVIE_OVERVIEW + " TEXT,"
+                    + MovieDatabaseContract.Movies.COLUMN_MOVIE_VOTE_AVERAGE + " REAL,"
+                    + MovieDatabaseContract.Movies.COLUMN_MOVIE_VOTE_COUNT + " INTEGER,"
+                    + MovieDatabaseContract.Movies.COLUMN_MOVIE_BACKDROP_PATH + " TEXT,"
+                    + MovieDatabaseContract.Movies.COLUMN_MOVIE_POSTER_PATH + " TEXT,"
+                    + MovieDatabaseContract.Movies.COLUMN_MOVIE_RELEASE_DATE + " TEXT,"
+                    + MovieDatabaseContract.Movies.COLUMN_MOVIE_FAVORED + " INTEGER NOT NULL DEFAULT 0,"
+                    + "UNIQUE (" + MovieDatabaseContract.Movies.COLUMN_MOVIE_ID + ") ON CONFLICT REPLACE)");
+
+        }
+
+        @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+
+        /**
+         * Delete the whole database.
+         */
+        void deleteDatabase() {
+            context.deleteDatabase(DB_NAME);
+        }
 }
--- app/src/main/java/id/developer/agungaprian/popularmovies/data/MovieProvider.java
+++ app/src/main/java/id/developer/agungaprian/popularmovies/data/MovieProvider.java
@@ -1,30 +1,143 @@
 package id.developer.agungaprian.popularmovies.data;
 
-import android.content.Context;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 
+import javax.inject.Inject;
+
+import static id.developer.agungaprian.popularmovies.data.MovieDatabaseContract.BASE_CONTENT_URI;
+import static id.developer.agungaprian.popularmovies.data.MovieDatabaseContract.CONTENT_AUTHORITY;
+import static id.developer.agungaprian.popularmovies.data.MovieDatabaseContract.Movies;
 /**
  * Created by agungaprian on 03/08/17.
  */
 
-public class MovieProvider extends SQLiteOpenHelper {
-    private static final String DB_NAME = "movies.db";
-    private static final int DB_VERSION = 1;
-    private final Context context;
+public class MovieProvider extends ContentProvider {
+    private static final int MOVIES = 100;
+    private static final int MOVIES_ID = 101;
+    private static final String MOVIES_PATH = "movies";
+    private static final String MOVIES_ID_PATH = "movies/*";
+    private static final UriMatcher URI_MATCHER = buildUriMatcher();
+    private MovieDbHelper movieHelper;
 
-    public MovieProvider(Context context) {
-        super(context, DB_NAME, null, DB_VERSION);
-        this.context = context;
+    @Inject public MovieProvider() {
+    }
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, MOVIES_PATH, MOVIES);
+        matcher.addURI(authority, MOVIES_ID_PATH, MOVIES_ID);
+
+        return matcher;
+    }
+
+    @Override public boolean onCreate() {
+        movieHelper = new MovieDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        final SQLiteDatabase db = movieHelper.getWritableDatabase();
+        final int match = URI_MATCHER.match(uri);
+
+        Cursor query;
+        switch (match) {
+            case MOVIES: {
+                query = db.query(Movies.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            default: {
+                throw new UnsupportedOperationException("Unknown insert uri: " + uri);
+            }
+        }
+        query.setNotificationUri(getContext().getContentResolver(), uri);
+        return query;
+    }
+
+    @Nullable @Override public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable @Override public Uri insert(Uri uri, ContentValues values) {
+        final SQLiteDatabase db = movieHelper.getWritableDatabase();
+        final int match = URI_MATCHER.match(uri);
+        Uri insertUri;
+        switch (match) {
+            case MOVIES: {
+                long id = db.insertOrThrow(Movies.TABLE_NAME, null, values);
+                insertUri = Movies.buildMovieUri(String.valueOf(id));
+                break;
+            }
+            default: {
+                throw new UnsupportedOperationException("Unknown insert uri: " + uri);
+            }
+        }
+        notifyResolver(uri);
+        return insertUri;
+    }
+
+    private void notifyResolver(Uri uri) {
+        getContext().getContentResolver().notifyChange(uri, null);
+    }
+
+    @Override public int delete(Uri uri, String selection, String[] selectionArgs) {
+        if (uri.equals(BASE_CONTENT_URI)) {
+            movieHelper.close();
+            movieHelper.deleteDatabase();
+            movieHelper = new MovieDbHelper(getContext());
+            return 1;
+        }
+        final SQLiteDatabase db = movieHelper.getWritableDatabase();
+        final int match = URI_MATCHER.match(uri);
+        int retrieveValue;
+        switch (match) {
+            case MOVIES: {
+                retrieveValue = db.delete(Movies.TABLE_NAME,
+                        selection,
+                        selectionArgs);
+                break;
+            }
+            default: {
+                throw new UnsupportedOperationException("Unknown insert uri: " + uri);
+            }
+        }
+        notifyResolver(uri);
+        return retrieveValue;
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = movieHelper.getWritableDatabase();
+        final int match = URI_MATCHER.match(uri);
+        int retrieveValue;
+        switch (match) {
+            case MOVIES: {
+                retrieveValue = db.update(Movies.TABLE_NAME,
+                        values,
+                        selection,
+                        selectionArgs);
+                break;
+            }
+            default: {
+                throw new UnsupportedOperationException("Unknown insert uri: " + uri);
+            }
+        }
+        notifyResolver(uri);
+        return retrieveValue;
     }
 }
